29beca42ca969460940a29b607739e57d7ed8a40
Esteban Gómez <esteban.gomezl@uqvirtual.edu.co>
2025-02-28 07:46:46 -0500

    asd

---
+++ src/main/java/com/example/ej1/Empresa.java
@@ -0,0 +1,5 @@
+package com.example.ej1;
+
+public class Empresa {
+    //Crear la lista de productos en una clase empresa utilizando treeset, se debe realizar un método que busque un producto por su código.
+}
---
+++ src/main/java/com/example/ej2/Objeto.java
@@ -0,0 +1,8 @@
+package com.example.ej2;
+
+import java.util.Stack;
+
+public class Objeto {
+    //Cree una pila (Stack) que pueda almacenar objetos de diferentes tipos y que solo permite insertar elementos si el tipo del objeto coincide con el tipo del elemento en la cima de la pila.
+    Stack<Object> pila = new Stack<Object>();
+}
